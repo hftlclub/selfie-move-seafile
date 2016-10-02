2c8cd1fcd8de83c7fe45f2bd6c4bd8c7081c8823
Ferdinand Malcher <mail@ferdinand-malcher.de>
2016-10-02 16:06:46 +0200

    Dont process if there are no selfies in the folder

--- moveSelfies.js
+++ moveSelfies.js
@@ -4,28 +4,41 @@ var config = require('./config.js');
 
 var sf = new SeafileAPI(config.url, config.token);
 
-
-// move folder from sync repo to final repo
-sf.moveMultiple({
-    src_repo: config.repos.sync,
-    dst_repo: config.repos.final,
-    file_names: [config.srcDir],
+// check whether there are file present
+sf.listDirEntries({
+    repo_id: config.repos.sync,
+    p: config.srcDir,
+    t: 'f',
 }, (err, body) => {
-    console.log(err, body);
-    if(err) return; 
+    if(!body.length) return; // cancel if no files are present
+    
+    // move folder from sync repo to final repo
+    sf.moveMultiple({
+        src_repo: config.repos.sync,
+        dst_repo: config.repos.final,
+        file_names: [config.srcDir],
+    }, (err, body) => {
+        console.log(err, body);
+        if(err) return; 
 
-    // rename folder in destination
-    let yesterday = moment().subtract(1, 'day').format('YYYY-MM-DD');
+        // rename folder in destination
+        let yesterday = moment().subtract(1, 'day').format('YYYY-MM-DD');
 
-    sf.renameDirectory({
-        repo_id: config.repos.final,
-        p: config.srcDir,
-        newname: yesterday
-    }, (err, body) => {console.log(err, body);});
+        sf.renameDirectory({
+            repo_id: config.repos.final,
+            p: config.srcDir,
+            newname: yesterday
+        }, (err, body) => {console.log(err, body);});
 
-    // create new source folder
-    sf.createDirectory({
-        repo_id: config.repos.sync,
-        p: config.srcDir
-    }, (err, body) => {console.log(err, body);});
+        // create new source folder
+        sf.createDirectory({
+            repo_id: config.repos.sync,
+            p: config.srcDir
+        }, (err, body) => {console.log(err, body);});
+    });
+
+
+
 });
+
+
